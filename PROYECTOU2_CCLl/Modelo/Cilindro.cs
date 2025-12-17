7717683f07426f3d9f4ec89166c7b2c1674b0348
eluce <ccelc7@gmail.com>
2025-12-17 16:49:16 -0500

    cono y cilindro

---
+++ PROYECTOU2_CCLl/Modelo/Cilindro.cs
@@ -0,0 +1,16 @@
+﻿namespace PROYECTO_U2_CCLl.Modelo
+{
+    public class Cilindro : Figura3D
+    {
+        public float Radio { get; set; }
+        public float Altura { get; set; }
+        public int Segmentos { get; set; }
+
+        public Cilindro(string nombre, float radio, float altura, int segmentos = 32) : base(nombre)
+        {
+            Radio = radio;
+            Altura = altura;
+            Segmentos = segmentos;
+        }
+    }
+}
---
+++ PROYECTOU2_CCLl/Modelo/Cono.cs
@@ -0,0 +1,16 @@
+﻿namespace PROYECTO_U2_CCLl.Modelo
+{
+    public class Cono : Figura3D
+    {
+        public float Radio { get; set; }
+        public float Altura { get; set; }
+        public int Segmentos { get; set; }
+
+        public Cono(string nombre, float radio, float altura, int segmentos = 32) : base(nombre)
+        {
+            Radio = radio;
+            Altura = altura;
+            Segmentos = segmentos;
+        }
+    }
+}
